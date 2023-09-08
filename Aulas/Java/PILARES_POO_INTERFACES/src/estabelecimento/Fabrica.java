3d2c0380ac6eb05923b66fe9f3de4314d3be818a
ryanBrittes <ryanbrittes027@gmail.com>
2023-09-08 08:17:14 -0400

    update

---
+++ Aulas/Java/PILARES_POO_INTERFACES/src/estabelecimento/Fabrica.java
@@ -0,0 +1,25 @@
+package estabelecimento;
+
+import equipamento.copiadora.Copiadora;
+import equipamento.digitalizadora.Digitalizadora;
+import equipamento.digitalizadora.Scanner;
+import equipamento.impressora.Deskjet;
+import equipamento.impressora.Impressora;
+import equipamento.impressora.Laserjet;
+import equipamento.multifuncional.EquipamentoMultifuncional;
+
+public class Fabrica {
+    public static void main(String[] args) {
+        EquipamentoMultifuncional em = new EquipamentoMultifuncional();
+
+        Scanner scanner = new Scanner();
+
+        Impressora impressora = em;
+        Digitalizadora digitalizadora = scanner;
+        Copiadora copiadora = em;
+
+        impressora.imprimir();
+        digitalizadora.digitalizar();
+        copiadora.copiar();
+    }
+}
